bb77a694dbb26afdd5ed5447fcbbd820534deded
xmy180719 <15281835886@163.com>
2019-07-28 18:10:47 +0800

    新增Test文件

---
+++ src/main/java/com/gupaoedu/vip/designmode/factory/abstractFactory/Test.java
@@ -0,0 +1,12 @@
+package com.gupaoedu.vip.designmode.factory.abstractFactory;
+
+/**
+ * Administrator
+ * 2019/7/28
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试新增");
+    }
+
+}
